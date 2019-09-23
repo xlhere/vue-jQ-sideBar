9fc80a70e95745a9b98a27ea1f230cdd84376c44
18158528064 <iforeach@163.com>
2019-09-23 15:07:21 +0800

    添加国际化

--- src/components/ImportFile.vue
+++ src/components/ImportFile.vue
@@ -1,16 +1,95 @@
 <template>
-  <div>
+  <div class="import-file">
+    <el-button @click="handleInsert" icon="el-icon-plus" size="mini" type="primary">添加</el-button>
 
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+      <el-form
+        :model="temp"
+        :rules="rules"
+        label-position="left"
+        label-width="70px"
+        ref="temp"
+        style="width: 400px; margin-left:50px;"
+      >
+        <el-form-item label="父节点" prop="superiorId">
+          <el-select placeholder="父节点" v-model="temp.superiorId">
+            <el-option
+              :key="item.id"
+              :label="item.label"
+              :value="item.id"
+              v-for="(item) in optionParentNode"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="部门" prop="name">
+          <el-input v-model="temp.name"></el-input>
+        </el-form-item>
+        <el-form-item label="备注" prop="description">
+          <el-input v-model="temp.description"></el-input>
+        </el-form-item>
+      </el-form>
+      <div class="dialog-footer" slot="footer">
+        <el-button @click="dialogFormVisible = false">{{$t('table.cancel')}}</el-button>
+        <el-button
+          @click="insertRow"
+          type="primary"
+          v-if="dialogStatus=='insert'"
+        >{{$t('table.confirm')}}</el-button>
+        <el-button @click="updateRow" type="primary" v-else>{{$t('table.confirm')}}</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-
+      optionParentNode:[],
+      temp: {
+        superiorId: "",
+        organizationStructureId: "",
+        name: "",
+        description: "",
+        amenderName: "系统管理员",
+        amendTime: ""
+      },
+      dialogFormVisible: false,
+      dialogStatus: "insert",
+      textMap: {
+        update: "修改",
+        insert: "新增"
+      },
+      rules: {
+        name: [{ required: true, message: '不能为空', trigger: 'blur' }],
+      },
     }
   },
+  methods: {
+    handleInsert(){
+      this.dialogFormVisible = true
+      this.dialogStatus = 'insert'
+      // this.resetTemp(null)
+    },
+    insertRow(){
+    this.$refs.temp.validate(valid => {
+      if(valid){
+        ajax('/zyfx-pc-web/organization/insert.do',this.temp
+        ).then(res=>{
+          this.dialogFormVisible = false
+          this.$message.success({message:res.message,duration:1500})
 
+          this.freshTree();
+        })
+      }
+    })
+  },
+  }
 }
 </script>
+
+<style lang="">
+.import-file {
+  padding: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import '@/styles/index.css'
 import XLSX from 'xlsx'
+import VueI18n from 'vue-i18n'
+import zh from './lang/zh'
+
+Vue.use(VueI18n)
+let lang = (navigator.languages && navigator.languages[0]) || navigator.language
+const i18n = new VueI18n({
+    locale: lang.split('-')[0],    // 语言标识
+    //this.$i18n.locale // 通过切换locale的值来实现语言切换
+    messages: {
+      zh
+    }
+})
 Vue.prototype.XLSX = XLSX
 
 Vue.use(Element, {
@@ -16,6 +29,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  i18n,
   router,
   components: { App },
   template: '<App/>'
