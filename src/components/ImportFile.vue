54c46dfbf1fdfb8376ca2e95943244079bcb7b2f
18158528064 <iforeach@163.com>
2019-10-16 13:07:14 +0800

    格式化代码

--- src/components/ImportFile.vue
+++ src/components/ImportFile.vue
@@ -2,53 +2,15 @@
   <div class="hello">
     {{msg}}
     <div>
-      <el-button
-        type="primary"
-        size="mini"
-        style="margin:30px auto;"
-        @click="handleDownloadData"
-      >导出</el-button>
-      <el-table
-        :data="tableData"
-        stripe
-        style="width: 600px;margin:30px auto;"
-      >
-        <el-table-column
-          prop="date"
-          label="日期"
-          width="100"
-        >
-        </el-table-column>
-        <el-table-column
-          prop="name"
-          label="姓名"
-          width="100"
-        >
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="地址"
-          width="190"
-        >
-        </el-table-column>
+      <el-button @click="handleDownloadData" size="mini" style="margin:30px auto;" type="primary">导出</el-button>
+      <el-table :data="tableData" stripe style="width: 600px;margin:30px auto;">
+        <el-table-column label="日期" prop="date" width="100"></el-table-column>
+        <el-table-column label="姓名" prop="name" width="100"></el-table-column>
+        <el-table-column label="地址" prop="address" width="190"></el-table-column>
 
-        <el-table-column
-          label="个人信息"
-          width="180"
-          align="center"
-        >
-          <el-table-column
-            prop="age"
-            label="年龄"
-            align="center"
-          >
-          </el-table-column>
-          <el-table-column
-            prop="height"
-            label="身高"
-            align="center"
-          >
-          </el-table-column>
+        <el-table-column align="center" label="个人信息" width="180">
+          <el-table-column align="center" label="年龄" prop="age"></el-table-column>
+          <el-table-column align="center" label="身高" prop="height"></el-table-column>
         </el-table-column>
       </el-table>
     </div>
@@ -57,45 +19,46 @@
 
 <script>
 export default {
-  name: 'excelExport',
+  name: "excelExport",
   data() {
     return {
-      msg: 'Welcome to Your Excel Export',
+      msg: "Welcome to Your Excel Export",
       tableData: [
         {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1510 弄',
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1510 弄",
           age: 18,
-          height: '180cm'
+          height: "180cm"
         },
         {
-          date: '2016-05-04',
-          name: '李小虎',
-          address: '上海市普陀区金沙江路 1511 弄',
+          date: "2016-05-04",
+          name: "李小虎",
+          address: "上海市普陀区金沙江路 1511 弄",
           age: 18,
-          height: '180cm'
-        }],
+          height: "180cm"
+        }
+      ],
       tableTitleData: [
         {
-          label: '日期',
-          prop: 'date'
+          label: "日期",
+          prop: "date"
         },
         {
-          label: '姓名',
-          prop: 'name'
+          label: "姓名",
+          prop: "name"
         },
         {
-          label: '地址',
-          prop: 'address'
+          label: "地址",
+          prop: "address"
         },
         {
-          label: '年龄',
-          prop: 'age'
+          label: "年龄",
+          prop: "age"
         },
         {
-          label: '身高',
-          prop: 'height'
+          label: "身高",
+          prop: "height"
         }
       ]
     }
@@ -111,13 +74,13 @@ export default {
         columnValues[i] = allColumns[i].prop
       }
       require.ensure([], () => {
-        const { export_json_to_excel } = require('vendor/Export2Excel')
+        const { export_json_to_excel } = require("vendor/Export2Excel")
         const tHeader = columnNames
         const filterVal = columnValues
 
         const list = this.tableData
         const data = this.formatJson(filterVal, list)
-        export_json_to_excel(tHeader, data, '导出excel列表demo')
+        export_json_to_excel(tHeader, data, "导出excel列表demo")
       })
     },
     formatJson(filterVal, jsonData) {
@@ -128,7 +91,7 @@ export default {
 </script>
 
 <style lang="">
-.hello{
+.hello {
   text-align: center;
 }
 </style>
