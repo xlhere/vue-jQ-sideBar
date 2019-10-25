c569985b918db77062250d7c2a8c51a6c7e4408d
18158528064 <iforeach@163.com>
2019-10-25 16:28:09 +0800

    更换logo

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -56,7 +56,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       inject: true,
-      favicon: 'static/logo.png'
+      favicon: 'static/logo-new.png'
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
